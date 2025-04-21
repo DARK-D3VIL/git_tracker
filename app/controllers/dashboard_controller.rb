5023485206ed4a92a20e8713ace8f92019e30eaa
Ketan singh <ketan02am@gmail.com>
2025-04-21 17:10:44 +0000

    small changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,35 +7,35 @@ class DashboardController < ApplicationController
     @dev_matrices = DeveloperMatrix.all
 
     time_range = nil
-    if params[:range].present?
-      if params[:range]=="7days"
+    if dashboard_params[:range].present?
+      if dashboard_params[:range] == "7days"
         time_range = Time.current - 7.days 
-      elsif params[:range]=="30days"
+      elsif dashboard_params[:range] == "30days"
         time_range = Time.current - 30.days
       end
     end
 
     status = "closed"
-    if params[:status].present? && params[:status]=="all"
-      status = params[:status]
+    if dashboard_params[:status].present? && dashboard_params[:status] == "all"
+      status = dashboard_params[:status]
     end
 
     sort_by = "dev_score"
-    if params[:sort_by].present? && params[:sort_by]=='rev_score'
-      sort_by = params[:sort_by]
+    if dashboard_params[:sort_by].present? && dashboard_params[:sort_by] == 'rev_score'
+      sort_by = dashboard_params[:sort_by]
     end
 
     if time_range.present?
-      @pull_requests = @pull_requests.where("pr_created_at >= ?",time_range)
-      @reviews = @reviews.where("rev_created_at >= ?",time_range)
+      @pull_requests = @pull_requests.where("pr_created_at >= ?", time_range)
+      @reviews = @reviews.where("rev_created_at >= ?", time_range)
     end
 
     if status == "closed"
       @pull_requests = @pull_requests.where(status: status)
     end
 
-    if params[:query].present?
-      query = params[:query].downcase
+    if dashboard_params[:query].present?
+      query = dashboard_params[:query].downcase
       @employees = Employee.where("LOWER(name) LIKE ?", "%#{query}%")
     end
 
@@ -49,15 +49,15 @@ class DashboardController < ApplicationController
     max_response_to_feedback = 0
 
     @employees.each do |emp|
-      MetricCalculate.new(emp,@reviews,@dev_matrices,@pull_requests).calculate
-      max_churn_score = [max_churn_score,emp.churn_score].max
-      max_merge_speed = [max_merge_speed,emp.merge_speed].max
-      max_code_quality = [max_code_quality,emp.code_quality].max
-      max_review_coverage = [max_review_coverage,emp.review_coverage].max
-      max_response_time = [max_response_time,emp.response_time].max
-      max_closing_speed = [max_closing_speed,emp.closing_speed].max
-      max_engagement_score = [max_engagement_score,emp.engagement_score].max
-      max_response_to_feedback = [max_response_to_feedback,emp.response_to_feedback].max
+      MetricCalculate.new(emp, @reviews, @dev_matrices, @pull_requests).calculate
+      max_churn_score = [max_churn_score, emp.churn_score].max
+      max_merge_speed = [max_merge_speed, emp.merge_speed].max
+      max_code_quality = [max_code_quality, emp.code_quality].max
+      max_review_coverage = [max_review_coverage, emp.review_coverage].max
+      max_response_time = [max_response_time, emp.response_time].max
+      max_closing_speed = [max_closing_speed, emp.closing_speed].max
+      max_engagement_score = [max_engagement_score, emp.engagement_score].max
+      max_response_to_feedback = [max_response_to_feedback, emp.response_to_feedback].max
     end
 
     @employees.each do |emp|
@@ -65,36 +65,35 @@ class DashboardController < ApplicationController
       final_rev_score = 0
 
       if max_churn_score > 0
-        final_dev_score += 0.15*(emp.churn_score/max_churn_score)
+        final_dev_score += 0.15 * (emp.churn_score / max_churn_score)
       end
       if max_code_quality > 0
-        final_dev_score += 0.30*(emp.code_quality/max_code_quality)
+        final_dev_score += 0.30 * (emp.code_quality / max_code_quality)
       end
       if max_merge_speed > 0 && emp.merge_speed > 0
-        final_dev_score += 0.30*(1 - (emp.merge_speed/max_merge_speed))
+        final_dev_score += 0.30 * (1 - (emp.merge_speed / max_merge_speed))
       end
       if max_response_to_feedback > 0 && emp.response_to_feedback > 0
-        final_dev_score += 0.25*(1 - (emp.response_to_feedback/max_response_to_feedback))
+        final_dev_score += 0.25 * (1 - (emp.response_to_feedback / max_response_to_feedback))
       end
 
       if max_review_coverage > 0
-        final_rev_score += 0.25*(emp.review_coverage/max_review_coverage)
+        final_rev_score += 0.25 * (emp.review_coverage / max_review_coverage)
       end
       if max_closing_speed > 0 && emp.closing_speed > 0
-        final_rev_score += 0.30*(1 - (emp.closing_speed/max_closing_speed))
+        final_rev_score += 0.30 * (1 - (emp.closing_speed / max_closing_speed))
       end
       if max_engagement_score > 0
-        final_rev_score += 0.25*(emp.engagement_score/max_engagement_score)
+        final_rev_score += 0.25 * (emp.engagement_score / max_engagement_score)
       end
       if max_response_time > 0 && emp.response_time > 0
-        final_rev_score += 0.20*(1-(emp.response_time/max_response_time))
+        final_rev_score += 0.20 * (1 - (emp.response_time / max_response_time))
       end
 
       emp.dev_score = final_dev_score
       emp.rev_score = final_rev_score
 
       emp.save!
-
     end
 
     @employees = @employees.sort_by(&sort_by.to_sym).reverse
@@ -103,17 +102,17 @@ class DashboardController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
     time_range = nil
-    if params[:range].present?
-      if params[:range]=="7days"
+    if dashboard_params[:range].present?
+      if dashboard_params[:range] == "7days"
         time_range = Time.current - 7.days 
-      elsif params[:range]=="30days"
+      elsif dashboard_params[:range] == "30days"
         time_range = Time.current - 30.days
       end
     end
 
     status = "closed"
-    if params[:status].present? && params[:status]=="all"
-      status = params[:status]
+    if dashboard_params[:status].present? && dashboard_params[:status] == "all"
+      status = dashboard_params[:status]
     end
 
     @pull_requests = PullRequest.all
@@ -127,7 +126,7 @@ class DashboardController < ApplicationController
     if status == "closed"
       @pull_requests = @pull_requests.where(status: status)
     end
-  
+    
     @dev_matrices = DeveloperMatrix.all 
     MetricCalculate.new(@employee, @reviews, @dev_matrices, @pull_requests).calculate
   end
@@ -138,5 +137,10 @@ class DashboardController < ApplicationController
     @prs = PullRequest.all
     @dev_matrices = DeveloperMatrix.all
   end
-  
+
+  private
+
+  def dashboard_params
+    params.permit(:range, :status, :sort_by, :query)
+  end
 end
